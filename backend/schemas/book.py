e8652e77b9cb26c318622875842887860954cb95
Naira98 <nairamm99@gmail.com>
2025-08-15 15:35:08 +0300

    fix: add session_id to alembic files again

--- backend/schemas/book.py
+++ backend/schemas/book.py
@@ -110,5 +110,4 @@ class BookDetailsForUpdateResponse(BaseModel):
         )
 
     class Config:
-        orm_mode = True
-        allow_population_by_field_name = True
+        from_attributes = True
